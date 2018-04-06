9a8647e74f434333ae2c186b9c05c14b13d83236
Shubham <shubh.mischieveous@gmail.com>
2018-04-06 20:25:49 +0530

    Shubham's models added

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var exphbs  = require('express-handlebars');
+var mongoose= require('mongoose');
 
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));

42750d2eab65d1217ce2bd98efb00bbba9b3657f
Shubhxotic <shubh.mischieveous@gmail.com>
2018-04-19 22:31:10 +0530

    Fixed the sub-navbar and authentication modal

--- routes/subcategories.js
+++ routes/subcategories.js
@@ -15,8 +15,32 @@ router.get('/all', function(req, res, next){
   });
 });
 
-router.get("/Subcats/:categoryId", function(req,res,next){
+
+router.get("/Category/:categoryId", function(req,res,next){
     db.Product.find({"Cat_id": req.params.categoryId}, function(err, Products){
+        if(err){
+            res.json(err);
+        }
+        // res.json(Products);
+
+        //Get the Subcategory Names
+        db.Subcategory.find({"Cat_id": req.params.categoryId}, function(err, Subcategories){
+            if(err){
+                res.json(err);
+            }
+            // res.json(Products);
+            
+            //Render the template   
+            res.render("subcat_filters",{Products: Products, Subcategories: Subcategories});
+        })
+        //Render the template
+        // res.render("subcat_filters",{Products: Products});
+    })
+})
+
+
+router.get("/Subcategory/:subcategoryId", function(req,res,next){
+    db.Product.find({"Subcat_id": req.params.subcategoryId}, function(err, Products){
         if(err){
             res.json(err);
         }
@@ -25,16 +49,15 @@ router.get("/Subcats/:categoryId", function(req,res,next){
     })
 })
 
-// Get Single Subcategory
-router.get('/Subcat/:id', function(req, res, next){
-  db.Subcategory.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, Subcategory){
-      if(err){
-          res.send(err);
-      }
-      res.json(Subcategory);
-  });
-});
-
+// // Get Single Subcategory
+// router.get('/Subcat/:id', function(req, res, next){
+//   db.Subcategory.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err, Subcategory){
+//       if(err){
+//           res.send(err);
+//       }
+//       res.json(Subcategory);
+//   });
+// });
 
 
 router.get('/add', function(req, res, next){
